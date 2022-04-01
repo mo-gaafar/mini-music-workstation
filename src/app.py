84c965a75046a011d32aacb20f443e978a41ef5e
Abdullahsaeed10 <14712019100085@stud.cu.edu.eg>
2022-04-02 01:50:18 +0200

    drums

--- src/app.py
+++ src/app.py
@@ -27,6 +27,7 @@ class MainWindow(QtWidgets.QMainWindow):
         # initialize arrays and variables
         self.music_signal = MusicSignal()
         self.piano_instrument= Piano()
+        self.drums_instrument= Drums()
         # initialize points to app
         self.pointsToAppend = 0
         interface.create_stacked_layout(self)
