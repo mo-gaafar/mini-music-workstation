fdcfc0c3c8d9cd9c0e9c894f7ee1b97deb5ca407
Mohamed N. Gaafar <46086535+mo-gaafar@users.noreply.github.com>
2022-04-05 02:30:03 +0200

    fix play

--- src/modules/instruments.py
+++ src/modules/instruments.py
@@ -18,6 +18,15 @@ class Instrument():
     # should contain the common general functions
     def __init__(self):
         pass
+
+    def float_to_int16(self, float_array):
+        int_array = np.int16(float_array * (32767/2))
+        return int_array
+
+    def play_sound(self, sound):
+        sound_object = pygame.sndarray.make_sound(array=sound)
+        sound_object.play()
+
     # Functions;
     # play notes
     # import base sample/tone
@@ -51,13 +60,8 @@ class Drums(Instrument):
 
     def play_drums(self, tone):
 
-        play_tone = random.choice(self.read_drum_tones[tone])
-
-        print_debug('play_tone  ')
-        print_debug(play_tone)
-
-        sound_object = pygame.sndarray.make_sound(array=play_tone)
-        sound_object.play()
+        play_sound = np.int16( random.choice(self.read_drum_tones[tone]))
+        self.play_sound(play_sound)
 
     def selecting_drum_kit(self, index):
         self.play_drums(index)
@@ -82,21 +86,20 @@ class Piano(Instrument):
         print(note_freq)
         return note_freq
 
-    def generating_wave(self,freq,duration=2.5):
-        time = np.linspace(0, duration, int(self.PIANO_SAMPLING_RATE * duration))
-        piano_wave = 0.6*np.sin(2 * np.pi * freq * time) * np.exp(-0.0015 * 2 * np.pi * freq * time)
-        #overtones 
-        piano_wave += 0.4*np.sin(2 * 2 * np.pi * freq * time) * np.exp(-0.0015 * 2 * np.pi * freq * time) / 2
-        
+    def generating_wave(self, freq, duration=2.5):
+        time = np.linspace(0, duration, int(
+            self.PIANO_SAMPLING_RATE * duration))
+        piano_wave = 0.6*np.sin(2 * np.pi * freq * time) * \
+            np.exp(-0.0015 * 2 * np.pi * freq * time)
+        # overtones
+        piano_wave += 0.4*np.sin(2 * 2 * np.pi * freq * time) * \
+            np.exp(-0.0015 * 2 * np.pi * freq * time) / 2
+
         piano_wave += piano_wave * piano_wave * piano_wave
         #piano_wave *= 1 + 16 * time * np.exp(-6 * time)
 
         piano_wave = self.float_to_int16(piano_wave)
         return piano_wave
-
-    def float_to_int16(self, float_array):
-        int_array = np.int16(float_array * (32767/2))
-        return int_array
 
     def play_note(self, input_note):
 
@@ -147,88 +150,81 @@ class Piano(Instrument):
         if key_index < 12:
             octave_number = self.octave_number
         else:
-            key_index= key_index - 12
-            octave_number= self.octave_number+1
-            
-        freq= self.key_freq(key_index,octave_number)
-        wave= self.generating_wave(freq,duration=3)
-        self.play_note(wave)
+            key_index = key_index - 12
+            octave_number = self.octave_number+1
+
+        freq = self.key_freq(key_index, octave_number)
+        wave = self.generating_wave(freq, duration=3)
+
+        self.play_sound(wave)
 
 
 class Guitar(Instrument):
     def __init__(self):
         super().__init__()
-        
-        self.GUITAR_SAMPLING_RATE=44100
-        self.guitar_chords={
-                        'G_major':[98,123,147,196,247,392]
-                        ,'D_major':[82,110,147,220,294,370]
-                        ,'C_major':[82,131,165,196,262,329]
-                        ,'E_major':[82,123,165,208,247,329]
-                        ,'A_major':[82,110,165,220,277,329]}
-        self.chord=self.guitar_chords['G_major'] #default
+
+        self.GUITAR_SAMPLING_RATE = 44100
+        self.guitar_chords = {
+            'G_major': [98, 123, 147, 196, 247, 392], 'D_major': [82, 110, 147, 220, 294, 370], 'C_major': [82, 131, 165, 196, 262, 329], 'E_major': [82, 123, 165, 208, 247, 329], 'A_major': [82, 110, 165, 220, 277, 329]}
+        self.chord = self.guitar_chords['G_major']  # default
         self.samples = []
-    def wavetable_initiator(self,string_pitch):
+
+    def wavetable_initiator(self, string_pitch):
         """Generates a new wavetable for the string."""
-        print('WAVETABLE_INIT')
-        print('pitch:')
+
         print(string_pitch)
         wavetable_size = self.GUITAR_SAMPLING_RATE // int(string_pitch)
-        print('wavetable_size:')
-        print(wavetable_size)
-        wavetable = (2 * np.random.randint(0, 2, wavetable_size) - 1).astype(np.float)
-        print('wavetable:')
-        print(wavetable)
+        wavetable = (2 * np.random.randint(0, 2,
+                     wavetable_size) - 1).astype(np.float)
         return wavetable
-      
-    def get_sample(self,wavetable, n_samples):
+
+    def get_sample(self, wavetable, n_samples):
         """Synthesizes a new waveform from an existing wavetable, modifies last sample by averaging."""
         current_sample = 0
         previous_value = 0
         samples = []
         while len(samples) < n_samples:
-            wavetable[current_sample] = 0.5 * (wavetable[current_sample] + previous_value)
+            wavetable[current_sample] = 0.5 * \
+                (wavetable[current_sample] + previous_value)
             samples.append(wavetable[current_sample])
             previous_value = samples[-1]
             current_sample += 1
             current_sample = current_sample % wavetable.size
         return np.array(samples)
 
-    def guitar_chord_selection(self,dial_number):
-        if dial_number ==1:
-            self.chord=self.guitar_chords['G_major']
-        elif dial_number ==2:
-            self.chord=self.guitar_chords['D_major']
-        elif dial_number ==3:
-            self.chord=self.guitar_chords['C_major']
-        elif dial_number ==4:
-            self.chord=self.guitar_chords['E_major']
-        elif dial_number ==5:
-            self.chord=self.guitar_chords['D_major']
+    def guitar_chord_selection(self, dial_number):
+        if dial_number == 1:
+            self.chord = self.guitar_chords['G_major']
+        elif dial_number == 2:
+            self.chord = self.guitar_chords['D_major']
+        elif dial_number == 3:
+            self.chord = self.guitar_chords['C_major']
+        elif dial_number == 4:
+            self.chord = self.guitar_chords['E_major']
+        elif dial_number == 5:
+            self.chord = self.guitar_chords['D_major']
 
     def guitar_chord_selection(self, dial_number):
 
         self.chord_number = self.guitar_chords['D_major']
 
     def play_string(self, sound):
-        print_debug(sound)
-        sound_object = pygame.sndarray.make_sound(array=sound)
-        sound_object.play()
+        sound = self.float_to_int16(sound)
+        self.play_sound(sound)
 
-    def guitar_string_sound(self,string_num):
+    def guitar_string_sound(self, string_num):
         print('chord:')
         print(self.chord)
         pitchs = self.chord
         print('freq:')
         print(pitchs[string_num])
-        #frequancy is the frequancy of string in the chosen chord
-        frequancy=pitchs[string_num]
+        # frequancy is the frequancy of string in the chosen chord
+        frequancy = pitchs[string_num]
         print('freq entering the equation:')
         print(frequancy)
         wave = self.wavetable_initiator(frequancy)
-        guitar_sound=self.get_sample(wave,self.GUITAR_SAMPLING_RATE * 5)
+        guitar_sound = self.get_sample(wave, self.GUITAR_SAMPLING_RATE * 5)
         self.play_string(guitar_sound)
-
 
 
 #TODO: connect in interface
